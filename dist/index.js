4c56f6aad17e1402d89a36ffffa39dfc35ab89e4
William Shackelford <bshackelford@gmail.com>
2016-03-31 20:07:08 -0400

    clean up prototype

--- dist/index.js
+++ dist/index.js
@@ -25,58 +25,79 @@
         };
     }
 
-    var Model = function Model(settings) {
-        var _this = this;
+    (function (EventEmitter, util, _) {
 
-        //EventEmitter.call(this);
-        this.settings = settings || {};
+        'use strict';
 
-        this.data = {};
+        var Model = function Model(settings) {
+            var _this = this;
 
-        this.init = function () {};
+            // a place to hold some settings
+            this.settings = settings || {};
 
-        this.set = function (data) {
-            //makeup a unqiue id
-            var id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
-            _this.data[id] = data;
-            _this.emit('change', _this.data);
-            return id;
+            // where the data is held for the model
+            this.data = {};
+
+            // this is called whenever the model is instantiated
+            this.init = function () {};
+
+            // the setter
+            this.set = function (data) {
+                //makeup a unqiue id
+                var id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
+                _this.data[id] = data;
+                _this.emit('change', _this.get());
+                return id;
+            };
+
+            // the getter
+            this.get = function (id) {
+                if (id) {
+                    return _this.data[id];
+                } else {
+                    //clean up protoype
+                    var name = void 0;
+                    var cleanData = {};
+                    for (name in _this.data) {
+                        if (_this.data.hasOwnProperty(name)) {
+                            cleanData[name] = _this.data[name];
+                        }
+                    }
+                    return cleanData;
+                }
+            };
+
+            // the updater
+            this.update = function (id, updateData) {
+                if (id && updateData && _this.data[id]) {
+                    _this.data[id] = _.extend({}, _this.data[id], updateData);
+                    _this.emit('change', _this.get());
+                    return _this.get(id);
+                } else {
+                    return;
+                }
+            };
+
+            // the deleter
+            this.delete = function (id) {
+                if (id) {
+                    delete _this.data[id];
+                    _this.emit('change', _this.get());
+                    return true;
+                } else {
+                    _this.data = {};
+                    _this.emit('change', _this.get());
+                    return _this.get();
+                }
+            };
+
+            // run it on instantiation
+            this.init();
         };
 
-        this.get = function (id) {
-            if (id) {
-                return _this.data[id];
-            } else {
-                return _this.data;
-            }
-        };
+        // this sets up the events
+        util.inherits(Model, EventEmitter);
 
-        this.update = function (id, updateData) {
-            if (id && updateData && _this.data[id]) {
-                _this.data[id] = _lodash2.default.extend({}, _this.data[id], updateData);
-                _this.emit('change', _this.data);
-                return _this.data[id];
-            } else {
-                return;
-            }
-        };
-
-        this.delete = function (id) {
-            if (id) {
-                delete _this.data[id];
-                _this.emit('change', _this.data);
-                return true;
-            } else {
-                _this.data = {};
-                _this.emit('change', _this.data);
-                return _this.data;
-            }
-        };
-
-        this.init();
-    };
-
-    _util2.default.inherits(Model, _events2.default);
-
-    module.exports = Model;
+        module.exports = Model;
+    })(_events2.default, _util2.default, _lodash2.default);
 });
--- src/index.js
+++ src/index.js
@@ -1,60 +1,80 @@
-'use strict';
-
 import EventEmitter from 'events';
 import util from 'util';
 import _ from 'lodash';
 
-const Model = function(settings) {
+(function(EventEmitter, util, _) {
 
-    //EventEmitter.call(this);
-    this.settings = settings || {};
+    'use strict';
 
-    this.data = {};
+    const Model = function(settings) {
 
-    this.init = () => {};
+        // a place to hold some settings
+        this.settings = settings || {};
 
-    this.set = data => {
-        //makeup a unqiue id
-        const id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
-        this.data[id] = data;
-        this.emit('change', this.data);
-        return id;
+        // where the data is held for the model
+        this.data = {};
+
+        // this is called whenever the model is instantiated
+        this.init = () => {};
+
+        // the setter
+        this.set = data => {
+            //makeup a unqiue id
+            const id = (+new Date() + Math.floor(Math.random() * 999999)).toString(36);
+            this.data[id] = data;
+            this.emit('change', this.get());
+            return id;
+        };
+
+        // the getter
+        this.get = id => {
+            if (id) {
+                return this.data[id];
+            } else {
+                //clean up protoype
+                let name;
+                const cleanData = {};
+                for (name in this.data) {
+                    if (this.data.hasOwnProperty(name)) {
+                        cleanData[name] = this.data[name];
+                    }
+                }
+                return cleanData;
+            }
+        };
+
+        // the updater
+        this.update = (id, updateData) => {
+            if (id && updateData && this.data[id]) {
+                this.data[id] = _.extend({}, this.data[id], updateData);
+                this.emit('change', this.get());
+                return this.get(id);
+            } else {
+                return;
+            }
+        };
+
+        // the deleter
+        this.delete = id => {
+            if (id) {
+                delete this.data[id];
+                this.emit('change', this.get());
+                return true;
+            } else {
+                this.data = {};
+                this.emit('change', this.get());
+                return this.get();
+            }
+        };
+
+        // run it on instantiation
+        this.init();
+
     };
 
-    this.get = id => {
-        if (id) {
-            return this.data[id];
-        } else {
-            return this.data;
-        }
-    };
+    // this sets up the events
+    util.inherits(Model, EventEmitter);
 
-    this.update = (id, updateData) => {
-        if (id && updateData && this.data[id]) {
-            this.data[id] = _.extend({}, this.data[id], updateData);
-            this.emit('change', this.data);
-            return this.data[id];
-        } else {
-            return;
-        }
-    };
+    module.exports = Model;
 
-    this.delete = id => {
-        if (id) {
-            delete this.data[id];
-            this.emit('change', this.data);
-            return true;
-        } else {
-            this.data = {};
-            this.emit('change', this.data);
-            return this.data;
-        }
-    };
-
-    this.init();
-
-};
-
-util.inherits(Model, EventEmitter);
-
-module.exports = Model;
+})(EventEmitter, util, _);
